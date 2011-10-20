cc8b1de695e96f6020bfb6462fb02292e0ab54e2
Bilal Akhtar <bilalakhtar@ubuntu.com>
2011-10-19 21:33:45 -0400

    Make app somewhat meaningful; get user to login and then store the crap

--- gen/com/bilal/schedulemytweets/R.java
+++ gen/com/bilal/schedulemytweets/R.java
@@ -17,6 +17,7 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int button_submit=0x7f060002;
         public static final int dropdown_duration=0x7f060001;
         public static final int edittext_tweet=0x7f060000;
     }
@@ -25,6 +26,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int button_submit_label=0x7f040005;
         public static final int dropdown_duration_prompt=0x7f040002;
         public static final int duration_textview_text=0x7f040003;
         public static final int edittext_tweet_hint=0x7f040004;
--- src/com/bilal/schedulemytweets/ScheduleMyTweetsActivity.java
+++ src/com/bilal/schedulemytweets/ScheduleMyTweetsActivity.java
@@ -3,23 +3,45 @@ package com.bilal.schedulemytweets;
 import java.util.*;
 import java.util.regex.Pattern;
 
+import oauth.signpost.*;
+import oauth.signpost.commonshttp.*;
+import oauth.signpost.basic.*;
+
 import android.app.Activity;
 import android.util.*;
 import android.view.*;
 import android.os.Bundle;
 import android.widget.*;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.content.*;
+import android.net.*;
 
 public class ScheduleMyTweetsActivity extends Activity implements OnItemSelectedListener {
 	private Hashtable<String,Integer> time_options = new Hashtable<String,Integer>();
 	
 	private String TAG="ScheduleMyTweets";
 	
+    private CommonsHttpOAuthConsumer OAuthConsumer;
+    private OAuthProvider OAuthProvider;
+	
+	private static final String consumer_key="ab1vkYWUfwk26ARsP0ckA";
+	private static final String consumer_secret="Ze6M5OdHMkdi1qfyGfk5MCy5t0NvcQzWTjnabTcTsHI";
+
+	private static final String CALLBACKURL = "app://schedulemytweets";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        SharedPreferences sp = getPreferences(MODE_PRIVATE);
+        
+        if (sp.contains("access_token") != true) {
+        	setup_twitter_auth();
+        } else {
+            Log.d(TAG,"Access token: " + sp.getString("access_token", "heynow") + "\nAccess Token Secret: " + sp.getString("access_token_secret", "beep"));
+        }
         
         Spinner dropdown_duration = (Spinner) findViewById(R.id.dropdown_duration);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -29,6 +51,66 @@ public class ScheduleMyTweetsActivity extends Activity implements OnItemSelected
         dropdown_duration.setOnItemSelectedListener(this);
         
         fill_time_options();
+    }
+    
+    private void setup_twitter_auth() {
+    	
+    	try {
+    		
+    	    OAuthConsumer = new CommonsHttpOAuthConsumer(consumer_key, consumer_secret);
+    	    OAuthProvider = new DefaultOAuthProvider("http://twitter.com/oauth/request_token",
+    	                                            "http://twitter.com/oauth/access_token",
+    	                                            "http://twitter.com/oauth/authorize");
+    	    
+    	    String authUrl = OAuthProvider.retrieveRequestToken(OAuthConsumer, CALLBACKURL);
+    	    
+    	    // Open the browser
+    	    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));
+    		
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+
+        Uri uri = intent.getData();
+        
+        if (settings.contains("access_token") == true) { // Already added key
+        	return;
+        }
+
+        //Check if you got NewIntent event due to Twitter Call back only
+
+        if (uri != null && uri.toString().startsWith(CALLBACKURL)) {
+
+            String verifier = uri.getQueryParameter(oauth.signpost.OAuth.OAUTH_VERIFIER);
+
+            try {
+                // this will populate token and token_secret in consumer
+
+                OAuthProvider.retrieveAccessToken(OAuthConsumer, verifier);
+                String userKey = OAuthConsumer.getToken();
+                String userSecret = OAuthConsumer.getTokenSecret();
+
+                // Save user_key and user_secret in user preferences and return
+
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("access_token", userKey);
+                editor.putString("access_token_secret", userSecret);
+                editor.commit();
+                
+                Log.d(TAG,"Access token: " + userKey + "\nAccess Token Secret: " + userSecret);
+
+            } catch(Exception e){
+            	e.printStackTrace();
+            }
+        }
+
     }
     
     private void fill_time_options() {
