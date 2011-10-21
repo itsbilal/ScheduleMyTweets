502e31173d138b8e63f3b3764289b10f0cf3f078
Bilal Akhtar <bilalakhtar@ubuntu.com>
2011-10-20 20:40:45 -0400

    Organize code in a better way, move twitter-specific crap to a new class

--- src/com/bilal/schedulemytweets/ScheduleMyTweetsActivity.java
+++ src/com/bilal/schedulemytweets/ScheduleMyTweetsActivity.java
@@ -3,19 +3,6 @@ package com.bilal.schedulemytweets;
 import java.util.*;
 import java.util.regex.Pattern;
 
-import oauth.signpost.*;
-import oauth.signpost.commonshttp.*;
-import oauth.signpost.basic.*;
-
-import org.apache.http.*;
-import org.apache.http.client.*;
-import org.apache.http.client.entity.*;
-import org.apache.http.impl.*;
-import org.apache.http.impl.client.*;
-import org.apache.http.client.methods.*;
-import org.apache.http.message.*;
-import org.apache.http.protocol.HTTP;
-
 import android.app.Activity;
 import android.util.*;
 import android.view.*;
@@ -30,13 +17,7 @@ public class ScheduleMyTweetsActivity extends Activity implements OnItemSelected
 	
 	private String TAG="ScheduleMyTweets";
 	
-    private CommonsHttpOAuthConsumer OAuthConsumer;
-    private OAuthProvider OAuthProvider;
-	
-	private static final String consumer_key="ab1vkYWUfwk26ARsP0ckA";
-	private static final String consumer_secret="Ze6M5OdHMkdi1qfyGfk5MCy5t0NvcQzWTjnabTcTsHI";
-
-	private static final String CALLBACKURL = "app://schedulemytweets";
+	private Twitter twitter_instance;
 	
     /** Called when the activity is first created. */
     @Override
@@ -47,11 +28,16 @@ public class ScheduleMyTweetsActivity extends Activity implements OnItemSelected
         SharedPreferences sp = getPreferences(MODE_PRIVATE);
         
         if (sp.contains("access_token") != true) {
-        	setup_twitter_auth();
+        	//setup_twitter_auth();
+        	twitter_instance = new Twitter(null, null);
+        	
+        	// Open OAuth login page
+        	startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(twitter_instance.setup_twitter_auth())));
         } else {
             Log.d(TAG,"Access token: " + sp.getString("access_token", "heynow") + "\nAccess Token Secret: " + sp.getString("access_token_secret", "beep"));
-            OAuthConsumer = new CommonsHttpOAuthConsumer(consumer_key, consumer_secret);
-            OAuthConsumer.setTokenWithSecret(sp.getString("access_token", "beep"), sp.getString("access_token_secret", "beep"));
+            //OAuthConsumer = new CommonsHttpOAuthConsumer(consumer_key, consumer_secret);
+            //OAuthConsumer.setTokenWithSecret(sp.getString("access_token", "beep"), sp.getString("access_token_secret", "beep"));
+            twitter_instance = new Twitter (sp.getString("access_token", "beep"), sp.getString("access_token_secret", "beep"));
         }
         
         Spinner dropdown_duration = (Spinner) findViewById(R.id.dropdown_duration);
@@ -61,27 +47,11 @@ public class ScheduleMyTweetsActivity extends Activity implements OnItemSelected
         dropdown_duration.setAdapter(adapter);
         dropdown_duration.setOnItemSelectedListener(this);
         
+        
+        
         fill_time_options();
     }
     
-    private void setup_twitter_auth() {
-    	
-    	try {
-    		
-    	    OAuthConsumer = new CommonsHttpOAuthConsumer(consumer_key, consumer_secret);
-    	    OAuthProvider = new DefaultOAuthProvider("http://twitter.com/oauth/request_token",
-    	                                            "http://twitter.com/oauth/access_token",
-    	                                            "http://twitter.com/oauth/authorize");
-    	    
-    	    String authUrl = OAuthProvider.retrieveRequestToken(OAuthConsumer, CALLBACKURL);
-    	    
-    	    // Open the browser
-    	    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));
-    		
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
-    }
     
     @Override
     protected void onNewIntent(Intent intent) {
@@ -91,37 +61,7 @@ public class ScheduleMyTweetsActivity extends Activity implements OnItemSelected
 
         Uri uri = intent.getData();
         
-        if (settings.contains("access_token") == true) { // Already added key
-        	return;
-        }
-
-        //Check if you got NewIntent event due to Twitter Call back only
-
-        if (uri != null && uri.toString().startsWith(CALLBACKURL)) {
-
-            String verifier = uri.getQueryParameter(oauth.signpost.OAuth.OAUTH_VERIFIER);
-
-            try {
-                // this will populate token and token_secret in consumer
-
-                OAuthProvider.retrieveAccessToken(OAuthConsumer, verifier);
-                String userKey = OAuthConsumer.getToken();
-                String userSecret = OAuthConsumer.getTokenSecret();
-
-                // Save user_key and user_secret in user preferences and return
-
-                SharedPreferences.Editor editor = settings.edit();
-                editor.putString("access_token", userKey);
-                editor.putString("access_token_secret", userSecret);
-                editor.commit();
-                
-                Log.d(TAG,"Access token: " + userKey + "\nAccess Token Secret: " + userSecret);
-
-            } catch(Exception e){
-            	e.printStackTrace();
-            }
-        }
-
+        twitter_instance.store_access_token(settings, uri);
     }
     
     private void fill_time_options() {
@@ -143,20 +83,7 @@ public class ScheduleMyTweetsActivity extends Activity implements OnItemSelected
     	String tweet = ((EditText)findViewById(R.id.edittext_tweet)).getText().toString();
     	
     	// TODO: This is just a placeholder to test tweeting
-    	try{
-	    	HttpClient http_client = new DefaultHttpClient();
-	    	HttpPost post_tweet = new HttpPost("http://api.twitter.com/1/statuses/update.json");
-	    	
-	    	List<NameValuePair> post_params = new ArrayList<NameValuePair>();
-	    	post_params.add(new BasicNameValuePair("status", tweet));
-	    	
-	    	post_tweet.setEntity(new UrlEncodedFormEntity(post_params, HTTP.UTF_8));
-	    	
-	    	OAuthConsumer.sign(post_tweet);
-	    	http_client.execute(post_tweet);
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
+    	twitter_instance.tweet(tweet);
     }
     
     public void onItemSelected(AdapterView<?> parent,
---
+++ src/com/bilal/schedulemytweets/Twitter.java
@@ -0,0 +1,117 @@
+package com.bilal.schedulemytweets;
+
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import oauth.signpost.*;
+import oauth.signpost.commonshttp.*;
+import oauth.signpost.basic.*;
+
+import org.apache.http.*;
+import org.apache.http.client.*;
+import org.apache.http.client.entity.*;
+import org.apache.http.impl.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.message.*;
+import org.apache.http.protocol.HTTP;
+
+import android.net.Uri;
+import android.util.Log;
+import android.content.*;
+
+public class Twitter {
+	
+	private CommonsHttpOAuthConsumer OAuthConsumer;
+    private OAuthProvider OAuthProvider;
+    
+	private static final String consumer_key="ab1vkYWUfwk26ARsP0ckA";
+	private static final String consumer_secret="Ze6M5OdHMkdi1qfyGfk5MCy5t0NvcQzWTjnabTcTsHI";
+
+	private static final String CALLBACKURL = "app://schedulemytweets";
+	
+	private static final String TAG="ScheduleMyTweets";
+	
+	public Twitter(String access_token, String access_token_secret) {
+		if (access_token != null) {
+    	    OAuthConsumer = new CommonsHttpOAuthConsumer(consumer_key, consumer_secret);
+    	    OAuthConsumer.setTokenWithSecret(access_token, access_token_secret);
+		}
+		
+	}
+	
+	public void store_access_token(SharedPreferences settings, Uri uri) {
+		if (settings.contains("access_token") == true) { // Already added key
+        	return;
+        }
+
+        //Check if you got NewIntent event due to Twitter Call back only
+
+        if (uri != null && uri.toString().startsWith(CALLBACKURL)) {
+
+            String verifier = uri.getQueryParameter(oauth.signpost.OAuth.OAUTH_VERIFIER);
+
+            try {
+                // Retrieve the access tokens
+
+                OAuthProvider.retrieveAccessToken(OAuthConsumer, verifier);
+                String userKey = OAuthConsumer.getToken();
+                String userSecret = OAuthConsumer.getTokenSecret();
+
+                // Save the access tokens in shared preferences
+
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("access_token", userKey);
+                editor.putString("access_token_secret", userSecret);
+                editor.commit();
+                
+                Log.d(TAG,"Access token: " + userKey + "\nAccess Token Secret: " + userSecret);
+
+            } catch(Exception e){
+            	e.printStackTrace();
+            }
+        }
+	}
+	
+    public String setup_twitter_auth() {
+    	
+    	try {
+    		
+    	    OAuthConsumer = new CommonsHttpOAuthConsumer(consumer_key, consumer_secret);
+    	    OAuthProvider = new DefaultOAuthProvider("http://twitter.com/oauth/request_token",
+    	                                            "http://twitter.com/oauth/access_token",
+    	                                            "http://twitter.com/oauth/authorize");
+    	    
+    	    String authUrl = OAuthProvider.retrieveRequestToken(OAuthConsumer, CALLBACKURL);
+    	    
+    	    // Open the browser
+    	    //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl)));
+    	    
+    	    return authUrl;
+    		
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+    }
+    
+    public void tweet(String tweet_text) {
+    	try{
+	    	HttpClient http_client = new DefaultHttpClient();
+	    	HttpPost post_tweet = new HttpPost("http://api.twitter.com/1/statuses/update.json");
+	    	
+	    	List<NameValuePair> post_params = new ArrayList<NameValuePair>();
+	    	post_params.add(new BasicNameValuePair("status", tweet_text));
+	    	
+	    	post_tweet.setEntity(new UrlEncodedFormEntity(post_params, HTTP.UTF_8));
+	    	
+	    	OAuthConsumer.sign(post_tweet);
+	    	http_client.execute(post_tweet);
+	    	
+	    	Log.d(TAG,"Tweeted: " + tweet_text);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+}
